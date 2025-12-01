c9d2a7e59a2b7e4bc42cafe04c18cb8452cd9c89
Tommymuia <muiathomas.mt@gmail.com>
2025-12-01 08:03:26 +0300

    added

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    """Print a Fibonacci sequence list of given length."""
+    if length <= 0:
+        print([])
+        return
+
+    if length == 1:
+        print([0])
+        return
+
+    # Start sequence
+    fib = [0, 1]
+
+    # Generate remaining numbers
+    for _ in range(2, length):
+        fib.append(fib[-1] + fib[-2])
+
+    print(fib)
